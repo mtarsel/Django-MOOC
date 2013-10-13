ff86e06523c319a9f10142a06445de770008f7a7
Mick Tarsel <mtarsel@gmail.com>
2013-10-13 18:50:50 -0400

    Added views and urls for files. We need to consider modularity - should start apps for different urls

--- mooc/urls.py
+++ mooc/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
-
-from mooc.views import login_view
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	url(r'^login/', login_view),
-	
-    # Examples:
-    # url(r'^$', 'mooc.views.home', name='home'),
-    # url(r'^mooc/', include('mooc.foo.urls')),
+from mooc.views import login_view
+from spine.views import file_upload
 
+urlpatterns = patterns('',
+    
+    url(r'^login/$', login_view),
+    url(r'^course/{/s}/files$', file_upload),
+#    url(r'^course/files/$', file_upload),
+	
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- spine/views.py
+++ spine/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, render_to_response
 from spine.models import Course
 
+def file_upload(request, course_id):    
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded = form.cleaned_data['file_upload']
+            mediaFile = Course(files=uploaded,
+                                owner=request.user.profile,
+                                creator=request.user.profile)
+            mediaFile.save()
 
+            return HttpResponseRedirect('/course/%s/' % course_id)#"%s/%d/%d/%s" % (settings.MEDIA_ROOT, course_id, user_id, filename)
+    else:
+        form = FileUploadForm()
+    return render_to_response('course/file_upload.html', {'form':form,'course':course}, context_instance=RequestContext(request))
