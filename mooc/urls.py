5e5efbab7f8ae1f022a76188ef0230f37aad045f
derek <dwklatt@gmail.com>
2013-10-13 22:06:03 -0400

    got the index.html to appear, just need to link it to the authentication

--- mooc/urls.py
+++ mooc/urls.py
@@ -2,18 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from mooc.views import login_view
+from mooc.views import login_view,student_view,instructor_view,login_fields
 from spine.views import file_upload
 
 urlpatterns = patterns('',
-    
-    url(r'^login/$', login_view),
+    url(r'^login/$', login_fields),
+    url(r'^login/next=?$', login_view),                    
     url(r'^student/$', student_view),
     url(r'^instructor/$', instructor_view),
     url(r'^course/{/s}/files$', file_upload),
 #    url(r'^course/files/$', file_upload),
-	
-    # Uncomment the admin/doc line below to enable admin documentation:
+# Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
