cff2e5458c71d58504a8de4868b4774face0ff55
Mick Tarsel <mtarsel@gmail.com>
2013-10-17 01:28:39 -0400

    Added comments to views for login only

--- student/views.py
+++ student/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-
+#@login_required
 def home(request):
     return render(request, 'student/home.html')
 
+#@login_required
 def courses(request):
     return render(request, 'student/courses.html')
 
