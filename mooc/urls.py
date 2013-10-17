8f31348a19fcc216499fb76acd1e3988d488ad2f
Mick Tarsel <mtarsel@gmail.com>
2013-10-17 00:12:20 -0400

    Added register page and updated comments

--- mooc/urls.py
+++ mooc/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', spine.views.index, name="index"),#index page
     url(r'^about/$', spine.views.about, name="about"),#information about MOOC
-    url(r'^login/$', spine.views.login, name="login"),#login page
+    url(r'^login/$', spine.views.login, name="login"),#login page for instructor and students
+    url(r'^register/$', spine.views.register, name="register"),#register page for new users
     url(r'^courses/$', spine.views.courses,name="courses"),#show all courses available
     
     url(r'^student/$', include('student.urls'), name="student"),#dashboard for student after login
