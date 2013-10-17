31692a1a11a85c686b6a9dafcea0753ddce9faf0
Mick Tarsel <mtarsel@gmail.com>
2013-10-17 00:05:16 -0400

    Updated urls with applications

--- mooc/urls.py
+++ mooc/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^student/$', include('student.urls'), name="student"),#home page for student after login
     url(r'^instructor/$', include('instructor.urls'), name="instructor"),
 
-
 # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
