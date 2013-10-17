23917ffb645d5725fad632cd661ac35e532dfbae
Mick Tarsel <mtarsel@gmail.com>
2013-10-16 22:31:12 -0400

    Renamed Instructor app to instructor. Updating URLs and creating a virual env.

--- mooc/settings.py
+++ mooc/settings.py
@@ -131,6 +131,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'spine',
+    'student',
+    'instructor',
 )
 
 # A sample logging configuration. The only tangible logging
--- mooc/urls.py
+++ mooc/urls.py
@@ -2,15 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from mooc.views import login_view,student_view,instructor_view,login_fields
-from spine.views import file_upload
 
 urlpatterns = patterns('',
-    url(r'^login/$', login_fields),
-    url(r'^login/next=?$', login_view),                    
-    url(r'^student/$', student_view),
-    url(r'^instructor/$', instructor_view),
-    url(r'^course/{/s}/files$', file_upload),
+    url(r'^login/$', mooc.views.login_fields, name="login"),#login page
+#    url(r'^login/next=?$', login_view),
+    url(r'^student/$', mooc.views.student, name="student"),#home page for student after login
+    url(r'^instructor/$', mooc.views.instructor_view, name="instructor"),
+#    url(r'^course/{/s}/files$', file_upload),
 #    url(r'^course/files/$', file_upload),
 # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
