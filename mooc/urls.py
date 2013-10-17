1f7655001faf2f0187d1c14b4e29a383e13452fd
Mick Tarsel <mtarsel@gmail.com>
2013-10-17 00:04:10 -0400

    Updated urls for urls in seperate apps

--- mooc/urls.py
+++ mooc/urls.py
@@ -2,17 +2,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-
 urlpatterns = patterns('',
-    url(r'^login/$', mooc.views.login_fields, name="login"),#login page
-#    url(r'^login/next=?$', login_view),
-    url(r'^student/$', mooc.views.student, name="student"),#home page for student after login
-    url(r'^instructor/$', mooc.views.instructor_view, name="instructor"),
-#    url(r'^course/{/s}/files$', file_upload),
-#    url(r'^course/files/$', file_upload),
+    url(r'^$', spine.views.index, name="index"),#index page
+    url(r'^about/$', spine.views.about, name="about"),#information about MOOC
+    url(r'^login/$', spine.views.login, name="login"),#login page
+    url(r'^courses/$', spine.views.courses,name="courses"),#show all courses available
+    
+    url(r'^student/$', include('student.urls'), name="student"),#home page for student after login
+    url(r'^instructor/$', include('instructor.urls'), name="instructor"),
+
+
 # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
