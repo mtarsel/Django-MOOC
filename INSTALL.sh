d18ff86e06defd81e926edf1aff2bbfdb21d2bc6
Mick Tarsel <mtarsel@gmail.com>
2013-10-16 22:37:36 -0400

    Install file added and renamed app

---
+++ INSTALL.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "Run as sudo"
+
+echo"Updating repos..."
+
+sudo apt-get install
+
+mkdir ${HOME}/software-d-and-d 
+cd ${HOME}/software-d-and-d
+
+echo "Created directory software-d-and-d in the home directory"
+
+echo "Creating virtual environment"
+
+virtualenv venv --distribute
+
+echo "Entering vm..."
+source venv/bin/activate
+
+echo "Changing directory"
+
+mkdir ${HOME}/software-d-and-d/dream-girlz
+
+cd ${HOME}/software-d-and-d/dream-girlz
+
+pwd
+
+echo "Pulling code and installing packages..."
+
+pip install django-toolbelt
+
+git init
+
+git pull https://github.com/mtarsel/dream-girlz.git
+
+echo "#######################################"
+echo "#######################################"
+echo "##     IMPORTANT INFORMATION	   ##"
+echo "##				   ##"
+echo "##    To activate virtualenv:        ##"
+echo "##				   ##"
+echo "##    source venv/bin/activate	   ##"
+echo "##				   ##"
+echo "##    DONT FORGET TO DELETE OLD	   ##"
+echo "##	   PROJECT		   ##"
+echo "##				   ##"
+echo "##	 You are now in		   ##"
+echo "##				   ##"
+echo "##    ~/software-d-and-d		   ##"
+echo "##				   ##"
+echo "##				   ##"
+echo "#######################################"
+echo "#######################################"
+
+
+cd ~/software-d-and-d/dream-girlz
+source venv/bin/activate
+
+
---
+++ instructor/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import authenticate, login, redirect, logout
+from django.shortcuts import render
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active and user.groups == Instructor:
+            login(request, user)
+            return render(request, 'mooc/templates/student.html'
+            # Redirect to a success page.
+        elif user.is_active and user.groups == Student:
+            login(request, user)
+            return render(request, 'mooc/templates/instructor.html'
+        else:
+           # Return a 'disabled account' error message
+    else:
+        # Return an 'invalid login' error message.
