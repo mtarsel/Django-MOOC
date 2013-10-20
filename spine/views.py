79c1d4574791ac5b4c9ccbd3fd6392bbd835812b
derek <dwklatt@gmail.com>
2013-10-20 17:18:18 -0400

    need to fix login and logout

--- spine/views.py
+++ spine/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from spine.models import Course
+from django.contrib.auth import logout
 
 def index(request):
     return render(request, 'index.html')
@@ -18,3 +19,6 @@ def courses(request):
     course_list = Course.objects.all()
     context = {'course_list': course_list}
     return render(request, 'courses.html', context)
+
+def logout_view( request ):
+    logout( request )
