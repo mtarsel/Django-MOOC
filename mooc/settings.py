818743a7632d4ea9c9e491ec6b7c773ab898acaf
Mick Tarsel <mtarsel@gmail.com>
2013-11-03 02:52:33 -0500

    Tested calendar app. Including new files next commit

--- mooc/settings.py
+++ mooc/settings.py
@@ -124,9 +124,20 @@ TEMPLATE_DIRS = (
     os.path.join( ROOT, 'templates' ),
 )
 
-TEMPLATE_CONTEXT_PROCESSORS =(
-    'django.core.context_processors.request',
-)
+#TEMPLATE_CONTEXT_PROCESSORS = (
+#    'django.core.context_processors.request',
+#)
+
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+        'django.contrib.auth.context_processors.auth',
+        "django.core.context_processors.debug",
+        "django.core.context_processors.i18n",
+        "django.core.context_processors.media",
+        "django.core.context_processors.request",
+        )
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
