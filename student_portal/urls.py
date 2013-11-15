5ae7a82394c7d8274d4f60a90d39f09d827696ef
Mick Tarsel <mtarsel@gmail.com>
2013-11-14 22:47:30 -0500

    Trying to add edit profile so student can initialize first last names

--- student_portal/urls.py
+++ student_portal/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('student_portal.views',
     url(r'^list/$', 'list', name='list'),
 
     #edit student profile 
-#    url(r'^edit_profile/$', auth(StudentProfileEditView.as_view()), name="edit_profile"),
+    url(r'^edit_profile/$', auth(StudentProfileEditView.as_view()), name="edit_profile"),
 
 # django registration simple, no email registration
     url(r'^accounts/', include('registration.backends.simple.urls')),
