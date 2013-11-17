4ab01bad9c2f3c2d62226e941096793051cf4028
derek <derek@localhost.localdomain>
2013-11-16 21:24:29 -0500

    a;lkdjfa;sdlkfja

--- student_portal/models.py
+++ student_portal/models.py
@@ -49,6 +49,7 @@ class Student(models.Model):
 
 class Assignment(models.Model):
     date = models.DateTimeField(editable=False, auto_now_add=True)
+    due_date = models.DateTimeField(editable=True)
     course = models.ForeignKey(Course)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=512)
