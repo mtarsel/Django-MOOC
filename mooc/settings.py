4c608785c182b085b1548af04a80b53e3b3a85df
Mick Tarsel <mtarsel@gmail.com>
2013-11-23 15:39:27 -0500

    Switched back to postresql

--- mooc/settings.py
+++ mooc/settings.py
@@ -21,13 +21,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mooc_database',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-#        'USER': 'liu',
-#        'PASSWORD': 'pa55word',
-#        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'liu',
+        'PASSWORD': 'pa55word',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
