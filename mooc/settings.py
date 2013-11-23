a3e4f2a44de784a8f4ef24f57b40e40180fee9c4
palmer <palmer.lao@gmail.com>
2013-11-22 19:48:29 -0500

    made the make_fake_data script to use: manage.py flush; this will clean your db and prompt you to make a new admin manage.py shell from make_fake_data import make_data; make_data(); this will add the following to your database: a bunch of users with password 1 one of them becomes an instructor everyone else is a student django class is created, and every student is enrolled in it one assignment of each kind is also created

--- mooc/settings.py
+++ mooc/settings.py
@@ -21,8 +21,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mooc_database',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'liu',
